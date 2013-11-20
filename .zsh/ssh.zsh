b141ad78373e1b2952fe165f8083764fb1677411
gifnksm <makoto.nksm+git@gmai.com>
2013-11-20 22:46:23 +0900

    Set ssh key lifetime

--- .zsh/ssh.zsh
+++ .zsh/ssh.zsh
@@ -1,20 +1,24 @@
+function ssh_add_key() {
+  ssh-add -l > /dev/null || ssh-add -t 1800 $@
+}
+
 function ssh_screen() {
     eval local server=\${$#}
-    ssh-add -l >/dev/null || ssh-add
+    ssh_add_key
     screen -t $server /usr/bin/ssh "$@"
 }
 
 function ssh_tmux() {
     eval local server=\${$#}
-    ssh-add -l >/dev/null || ssh-add
+    ssh_add_key
     tmux -q \
-        set set-remain-on-exit on\; \
-        new-window -n "${server}" "/usr/bin/ssh $@" \; \
+        set set-remain-on-exit on \; \
+        new-window -n "${server}" "TERM=xterm-256color /usr/bin/ssh $@" \; \
         set set-remain-on-exit off
 }
 
 function ssh_with_key() {
-    ssh-add -l >/dev/null || ssh-add
+    ssh_add_key
     /usr/bin/ssh $@
 }
 
