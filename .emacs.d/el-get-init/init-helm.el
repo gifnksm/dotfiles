25bdc8b4006593d857746bce1d9b67a2ceb135de
gifnksm <makoto.nksm+git@gmai.com>
2018-12-10 14:07:11 +0900

    emacs.d: modify helm key bindings

--- .emacs.d/el-get-init/init-helm.el
+++ .emacs.d/el-get-init/init-helm.el
@@ -4,8 +4,10 @@
     ("C-M-y" . helm-show-kill-ring)
     ("C-M-z" . helm-resume)))
 
-;; (define-key helm-map (kbd "C-h") 'delete-backward-char)
-;; (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(eval-after-load "helm"
+  '(define-key helm-map (kbd "C-h") 'delete-backward-char))
+(eval-after-load "helm-files"
+  '(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char))
 
 (el-get 'sync 'popwin)
 (when (featurep 'popwin)
