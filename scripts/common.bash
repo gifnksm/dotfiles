deaa7e09723e933bb25217d185cf4a955a506e87
gifnksm <makoto.nksm+github@gmail.com>
2023-07-29 14:30:50 +0900

    Refactor package installation

--- scripts/common.bash
+++ scripts/common.bash
@@ -277,28 +277,150 @@ ensure_git_config() {
 }
 
 pacman_install() {
+    [[ "$#" -eq 0 ]] && return
     info "pacman install: $*"
     sudo pacman -S --needed --noconfirm --color always "$@"
 }
 
 paru_install() {
+    [[ "$#" -eq 0 ]] && return
+
+    source scripts/install_paru.bash
+
     info "paru install: $*"
     paru -S --needed --noconfirm --color always "$@"
 }
 
 cargo_install() {
+    [[ "$#" -eq 0 ]] && return
+
+    source scripts/install_rustup.bash
+    source scripts/install_cargo_binstall.bash
+
     info "cargo binstall: $*"
     cargo binstall -y "$@"
 }
 
 apt_get_install() {
+    [[ "$#" -eq 0 ]] && return
+
     info "apt-get install: $*"
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends "$@"
 }
 
 dnf_install() {
+    [[ "$#" -eq 0 ]] && return
+
     info "dnf install: $*"
     sudo dnf install -y "$@"
+}
+
+_extract_package() {
+    cut -d@ -f1 <<<"${1}"
+}
+_extract_source() {
+    cut -d@ -f2- -s <<<"${1}"
+}
+
+install_package_arch() {
+    local package_and_sources=("$@")
+    local pacman_packages=()
+    local aur_packages=()
+    local cargo_packages=()
+
+    for package_and_source in "${package_and_sources[@]}"; do
+        if [[ -z "${package_and_source}" ]]; then
+            continue
+        fi
+
+        local package source
+        package="$(_extract_package "${package_and_source}")"
+        source="$(_extract_source "${package_and_source}")"
+
+        case "${source}" in
+        "" | pacman)
+            pacman_packages+=("${package}")
+            ;;
+        aur)
+            aur_packages+=("${package}")
+            ;;
+        cargo)
+            cargo_packages+=("${package}")
+            ;;
+        *)
+            error "invalid source '${source}' for package '${package}'"
+            return "${ERROR_EXIT_CODE}"
+            ;;
+        esac
+    done
+
+    pacman_install "${pacman_packages[@]}"
+    paru_install "${aur_packages[@]}"
+    cargo_install "${cargo_packages[@]}"
+}
+
+install_package_debian() {
+    local package_and_sources=("$@")
+    local apt_get_packages=()
+    local cargo_packages=()
+
+    for package_and_source in "${package_and_sources[@]}"; do
+        if [[ -z "${package_and_source}" ]]; then
+            continue
+        fi
+
+        local package source
+        package="$(_extract_package "${package_and_source}")"
+        source="$(_extract_source "${package_and_source}")"
+
+        case "${source}" in
+        "" | apt-get)
+            apt_get_packages+=("${package}")
+            ;;
+        cargo)
+            cargo_packages+=("${package}")
+            ;;
+        *)
+            error "invalid source '${source}' for package '${package}'"
+            return "${ERROR_EXIT_CODE}"
+            ;;
+        esac
+    done
+
+    apt_get_install "${apt_get_packages[@]}"
+    cargo_install "${cargo_packages[@]}"
+}
+
+install_package_rhel() {
+    local package_and_sources=("$@")
+    local dnf_packages=()
+    local cargo_packages=()
+
+    for package_and_source in "${package_and_sources[@]}"; do
+        if [[ -z "${package_and_source}" ]]; then
+            continue
+        fi
+
+        local package source
+        package="$(_extract_package "${package_and_source}")"
+        source="$(_extract_source "${package_and_source}")"
+
+        case "${source}" in
+        "" | dnf)
+            dnf_packages+=("${package}")
+            ;;
+        cargo)
+            cargo_packages+=("${package}")
+            ;;
+        *)
+            error "invalid source '${source}' for package '${package}'"
+            return "${ERROR_EXIT_CODE}"
+            ;;
+        esac
+    done
+
+    dnf_install "${dnf_packages[@]}"
+    cargo_install "${cargo_packages[@]}"
 }
 
 # Usage: install_package_by_spec <<END
@@ -330,75 +452,20 @@ install_package_by_spec() {
             continue
         fi
 
-        local package_and_source
-        for package_and_source in "${package_and_sources[@]}"; do
-            if [[ -z "${package_and_source}" ]]; then
-                continue
-            fi
-
-            local package source
-            package="$(cut -d@ -f1 <<<"${package_and_source}")"
-            source="$(cut -d@ -f2- -s <<<"${package_and_source}")"
-            if [[ -z "${package}" ]]; then
-                error "invalid package spec: ${spec}"
-                return "${ERROR_EXIT_CODE}"
-            fi
-
-            case "${os}" in
-            "${OS_ARCH}")
-                case "${source}" in
-                "" | "pacman")
-                    pacman_install "${package}"
-                    ;;
-                "aur")
-                    source scripts/install_paru.bash
-                    paru_install "${package}"
-                    ;;
-                "cargo")
-                    source scripts/install_rustup.bash
-                    cargo_install "${package}"
-                    ;;
-                *)
-                    error "invalid source '${source}' for package '${package}' in spec: ${spec}"
-                    return "${ERROR_EXIT_CODE}"
-                    ;;
-                esac
-                ;;
-            "${OS_UBUNTU_22_04}")
-                case "${source}" in
-                "" | "apt-get")
-                    apt_get_install "${package}"
-                    ;;
-                "cargo")
-                    source scripts/install_rustup.bash
-                    cargo_install "${package}"
-                    ;;
-                *)
-                    error "invalid source '${source}' for package '${package}' in spec: ${spec}"
-                    return "${ERROR_EXIT_CODE}"
-                    ;;
-                esac
-                ;;
-            "${OS_ROCKY_9}")
-                case "${source}" in
-                "" | "dnf")
-                    dnf_install "${package}"
-                    ;;
-                "cargo")
-                    source scripts/install_rustup.bash
-                    cargo_install "${package}"
-                    ;;
-                *)
-                    error "invalid source '${source}' for package '${package}' in spec: ${spec}"
-                    return "${ERROR_EXIT_CODE}"
-                    ;;
-                esac
-                ;;
-            *)
-                abort "unexpected error: os=${os}, spec=${spec}"
-                ;;
-            esac
-        done
+        case "${os}" in
+        "${OS_ARCH}")
+            install_package_arch "${package_and_sources[@]}"
+            ;;
+        "${OS_UBUNTU_22_04}")
+            install_package_debian "${package_and_sources[@]}"
+            ;;
+        "${OS_ROCKY_9}")
+            install_package_rhel "${package_and_sources[@]}"
+            ;;
+        *)
+            abort "unexpected error: os=${os}, spec=${spec}"
+            ;;
+        esac
 
         installed=true
     done
