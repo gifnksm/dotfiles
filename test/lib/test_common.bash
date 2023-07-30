1a5dd7c0f9fac2982546ebc79dd8dab31bbec575
gifnksm <makoto.nksm+github@gmail.com>
2023-07-30 11:50:54 +0900

    test: move docker files to test/docker

--- test/lib/test_common.bash
+++ test/lib/test_common.bash
@@ -10,7 +10,7 @@ run_test() {
     local -r test_os_name="${1}"
 
     local -r docker_image_name="dotfiles-test-${test_os_name}"
-    local -r dockerfile="${TEST_DIR}/Dockerfile.${test_os_name}"
+    local -r dockerfile="${TEST_DIR}/docker/Dockerfile.${test_os_name}"
 
     docker build --pull -t "${docker_image_name}" -f "${dockerfile}" .
     docker run --rm -v "${REPO_DIR}:/dotfiles" "${docker_image_name}" /dotfiles/install
