94863c18ce1f62910f10439fc0c0127958222a4d
gifnksm <makoto.nksm+github@gmail.com>
2023-08-27 17:06:11 +0900

    Change zsh history path

--- modules/zsh.bash
+++ modules/zsh.bash
@@ -13,11 +13,18 @@ END
 
     sudo chsh -s /bin/zsh "$(id -un)"
 
+    # zshrc
     ensure_symlink_to_config_not_exists .zshrc # this line must be before the next line
     ensure_line_in_file "source ~/.config/zsh/zshrc" ~/.zshrc
 
+    # zsh config files
     ensure_symlink_to_config_exists .config/zsh
-
     ensure_symlink_to_config_not_exists .zsh
+
+    # zsh history
+    ensure_directory_exists ~/.local/share/zsh
+    if [ -f ~/.config/zsh/history ]; then
+        mv ~/.config/zsh/history ~/.local/share/zsh/history
+    fi
 }
 group_end
