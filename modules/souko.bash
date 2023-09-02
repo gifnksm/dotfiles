cf65fc6fefa42a12c212595dac5cbe44c7cc2a8e
gifnksm <makoto.nksm+github@gmail.com>
2023-09-02 22:07:30 +0900

    Migrate from rhq to souko

---
+++ modules/souko.bash
@@ -0,0 +1,14 @@
+# shellcheck source-path=SCRIPTDIR/..
+is_executed && return
+
+group_start_file
+{
+    install_package_by_spec <<END
+        arch: souko-bin@aur
+        ubuntu-22.04: souko@cargo
+        rocky-9: souko@cargo
+END
+
+    assert_command souko
+}
+group_end
