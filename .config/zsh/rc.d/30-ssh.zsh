8c19e1addb42f6a95b4f1c688ee4932b26bd17c9
gifnksm <makoto.nksm+github@gmail.com>
2023-09-11 20:26:18 +0900

    tmux: define ssh_tmux only when zsh executed in tmux session

--- .config/zsh/rc.d/30-ssh.zsh
+++ .config/zsh/rc.d/30-ssh.zsh
@@ -1,16 +1,16 @@
-function ssh_tmux() {
-    local cmd="${argv}"
-    tmux -q \
-        set-hook window-linked 'set remain-on-exit on' \; \
-        new-window -n "${cmd}" "TERM=xterm-256color /usr/bin/ssh $cmd"
-    tmux -q \
-        set-hook -u window-linked
-}
-
 if command -v keychain >/dev/null; then
     eval "$(keychain --nogui --eval --agents ssh --timeout 30 -q)"
 fi
 
 if [ -n "$TMUX" ]; then
+    function ssh_tmux() {
+        local cmd="${argv}"
+        tmux -q \
+            set-hook window-linked 'set remain-on-exit on' \; \
+            new-window -n "${cmd}" "TERM=xterm-256color /usr/bin/ssh $cmd"
+        tmux -q \
+            set-hook -u window-linked
+    }
+
     alias ssh=ssh_tmux
 fi
