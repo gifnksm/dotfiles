451685048d9f2fb6575317997487e1921212d2ff
gifnksm <makoto.nksm+github@gmail.com>
2023-09-12 13:04:59 +0900

    Add sheldon
    fixes #31

---
+++ modules/sheldon.bash
@@ -0,0 +1,11 @@
+# shellcheck source-path=SCRIPTDIR/..
+
+install_package_by_spec <<END
+    arch: sheldon
+    ubuntu-22.04: sheldon@cargo
+    rocky-9: sheldon@cargo
+END
+
+assert_command sheldon
+
+ensure_symlink_to_config_exists .config/sheldon
