d068891567f2c71336b2369e87375bad038a916b
gifnksm <makoto.nksm+github@gmail.com>
2023-09-17 10:47:56 +0900

    zsh: `Simplify sheldon source

--- .config/zsh/rc.d/60-sheldon.zsh
+++ .config/zsh/rc.d/60-sheldon.zsh
@@ -1,8 +1,3 @@
 if command -v sheldon >/dev/null; then
-    sheldon() {
-        if command sheldon "$@"; then
-            eval "$(command sheldon source)"
-        fi
-    }
-    eval "$(command sheldon source)"
+    eval "$(sheldon source)"
 fi
