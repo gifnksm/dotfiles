81426cee0580062bf6016853743897e3f4ee9b16
gifnksm <makoto.nksm+github@gmail.com>
2023-09-17 13:14:27 +0900

    Improve zsh-tmux integration

--- .config/zsh/rc.d/40-tmux.zsh
+++ .config/zsh/rc.d/40-tmux.zsh
@@ -1,9 +1,18 @@
-function _update_tmux_env() {
-    unset $(tmux show-env | sed -n 's/^-//p')
-    eval export $(tmux show-env | sed -n 's/$/"/; s/=/="/p') >/dev/null
-}
+if [[ -n "${TMUX}" ]]; then
+    function _update_tmux_env() {
+        local -a lines=("${(f)$(tmux show-env)}")
+        local line
+        for line in "${lines[@]}"; do
+            if [[ "${line}" =~ ^- ]]; then
+                unset "${line:1}"
+            else
+                local name="${line%%=*}"
+                local value="${line#*=}"
+                export "${name}"="${value}"
+            fi
+        done
+    }
 
-if [ -n "${TMUX}" ]; then
     autoload -Uz add-zsh-hook
     add-zsh-hook preexec _update_tmux_env
 fi
