497eef82dc9f67d3a45c074c60843feea3368e27
gifnksm <makoto.nksm+github@gmail.com>
2023-09-21 16:40:34 +0900

    zsh: fix fzf-history-search behavior

--- .config/zsh/plugins/fzf-history-search/fzf-history-search.plugin.zsh
+++ .config/zsh/plugins/fzf-history-search/fzf-history-search.plugin.zsh
@@ -1,5 +1,5 @@
 function fzf-history-search() {
-    local candidates=(${(f)"$(builtin history -n -r 1 | fzf-tmux -p -w100% -h100% -- --reverse --query="${LBUFFER}" --prompt="History>")"})
+    local candidates=(${(f)"$(builtin history -n -r 1 | fzf-tmux -p -w100% -h100% -- --reverse --no-sort --query="${LBUFFER}" --prompt="History>")"})
     local ret=$?
     if [[ -n "$candidates" ]]; then
         BUFFER="${candidates[*]}"
