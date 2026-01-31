c06cedf7d6ad419af191785dcb2f8d0a57019025
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2026-01-31 17:22:01 +0900

    Update paru installation source from paru-bin to paru

--- modules/paru.bash
+++ modules/paru.bash
@@ -5,8 +5,8 @@ if [[ "${OS_ID}" == "${OS_ARCH}" ]] && ! command -v paru >/dev/null; then
 
     ensure_directory_exists ~/.cache/paru/clone
 
-    ACTION="fetching paru-bin" execute git clone -q https://aur.archlinux.org/paru-bin.git ~/.cache/paru/clone/paru-bin
-    ACTION="installing paru-bin" execute bash -c "cd ~/.cache/paru/clone/paru-bin && makepkg -si --noconfirm"
+    ACTION="fetching paru-bin" execute git clone -q https://aur.archlinux.org/paru.git ~/.cache/paru/clone/paru-bin
+    ACTION="installing paru-bin" execute bash -c "cd ~/.cache/paru/clone/paru && makepkg -si --noconfirm"
 
     assert_command paru
 fi
