f85d65b67b2218c24d8cf807b7ccfc234f1b4719
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2026-01-31 17:25:20 +0900

    Fix clone directory for paru-bin installation

--- modules/paru.bash
+++ modules/paru.bash
@@ -5,7 +5,7 @@ if [[ "${OS_ID}" == "${OS_ARCH}" ]] && ! command -v paru >/dev/null; then
 
     ensure_directory_exists ~/.cache/paru/clone
 
-    ACTION="fetching paru-bin" execute git clone -q https://aur.archlinux.org/paru.git ~/.cache/paru/clone/paru-bin
+    ACTION="fetching paru-bin" execute git clone -q https://aur.archlinux.org/paru.git ~/.cache/paru/clone/paru
     ACTION="installing paru-bin" execute bash -c "cd ~/.cache/paru/clone/paru && makepkg -si --noconfirm"
 
     assert_command paru
